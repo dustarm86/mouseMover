3f4bc3cb09ca3b46452fdd5646bc9438b38e24a1
Dustin Armstrong <dieter@Sandpaws-Macbook-Pro.local>
2019-09-27 19:37:48 -0400

    added 10 sec timer to position mouse after initial launch

--- mouseMover.py
+++ mouseMover.py
@@ -14,11 +14,12 @@ print('mouseMover prog will run for', userTime, 'hour(s)')
 startTime = time.time() # remember when we started
 
 while (time.time() - startTime) < (userTime * 60) * 60:
+    pyautogui.PAUSE = 10
     pyautogui.moveRel(0, 10)
     pyautogui.click()
-    pyautogui.PAUSE = 60
+    pyautogui.PAUSE = 15
     pyautogui.moveRel(0, -10)
     pyautogui.click()
-    pyautogui.PAUSE = 60
+    pyautogui.PAUSE = 15
 
 print('mouseMover prog has finished running for', userTime, 'hour(s)')
